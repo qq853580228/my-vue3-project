49351f2425face67cc75867f1a208151a3337b48
zjy <853580228@qq.com>
2024-03-21 18:01:53 +0800

    test

--- src/api/base.js
+++ src/api/base.js
@@ -2,8 +2,9 @@
  * @Author: zjy 853580228@qq.com
  * @Date: 2024-03-21 16:41:38
  * @LastEditors: zjy 853580228@qq.com
- * @LastEditTime: 2024-03-21 17:46:29
+ * @LastEditTime: 2024-03-21 18:01:47
  * @FilePath: \my-vue3-project\src\api\base.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const LOGIN_PATH = '/login';
+export const INDEX_PATH = '/index';
